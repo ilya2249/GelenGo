97db82a8a88b8e6f7572132e198526d787a7640f
Ilya_Vasiliev <ilya.vas.08@mail.ru>
2024-01-07 20:45:59 +0300

    modified:   GelenGo/settings.py

--- GelenGo/settings.py
+++ GelenGo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1=g@ew*psdd^_9f-1_dwt(59d#doq-g(1f&zs(1tur^vt&!*7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
